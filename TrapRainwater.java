e2a0bbdbb1d7b39baee734af0311b65c20ae564f
ZhouCihang <matthewzhou1128@gmail.com>
2020-08-24 17:13:34 +0800

    initial commit

---
+++ TrapRainwater.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class TrapRainwater{
+    public static void main(String[] args) {
+        int inputLength;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your array size:");
+        inputLength = in.nextInt();
+
+        int[] input = new int[inputLength];
+        for (int i = 0; i <inputLength; i++){
+            input[i] = in.nextInt();
+        }
+
+    }
+}
